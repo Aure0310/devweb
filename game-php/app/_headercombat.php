aee23ce1abb095c3e3b90e81592fc83a55acbd57
Aurélien <90250923+Aure0310@users.noreply.github.com>
2023-05-05 17:15:56 +0200

    Add files via upload

---
+++ game-php/app/_headercombat.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/donjons.css" />
+    <link rel="stylesheet" href="styles/combat.css" />
+    <link rel="stylesheet" href="styles/nav.css" />
+    
+</head>
+<body>
+    <?php require_once('_nav.php'); ?>
---
+++ game-php/app/classes/Ennemi.php
@@ -0,0 +1,17 @@
+<?php
+
+class Ennemi
+{
+    public int $pol;
+    public string $name;
+    public int $power;
+    public int $constitution;
+    public int $speed;
+    public int $xp;
+    public int $gold;
+
+    public function atk()
+    {
+
+    }
+}
